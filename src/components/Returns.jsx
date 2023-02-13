6b672658fa6752cc2b2eb1168fee0cc7aedde4bd
Pushkar Dangi <pushkardangi746@gmail.com>
2023-02-13 12:06:58 +0530

    fixes in component:returns

--- src/components/Returns.jsx
+++ src/components/Returns.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 function Returns() {
 
-  const calcAvg = (event) => {
+  const calcReturns = (event) => {
 
     let shares = parseInt(document.getElementById("totalShares").value);
-    let returns = parseFloat(document.getElementById("returns").value);
+    let currentVal = parseFloat(document.getElementById("currentVal").value);
+    let avgVal = parseFloat(document.getElementById("avgVal").value);
 
     console.log(shares);
-    console.log(returns);
+    console.log(currentVal);
+    console.log(avgVal);
+
+    let returns = (shares*currentVal) - (shares*avgVal);
 
     document.getElementById("avgBox").innerHTML = returns.toFixed(2);
 
@@ -17,7 +21,6 @@ function Returns() {
 
   return (
     <>
-      <form>
         <div className="my-4">
           <label className="block">
             <input
@@ -25,6 +28,7 @@ function Returns() {
               id="totalShares"
               className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="Enter number of stocks"
+              defaultValue={0}
             />
           </label>
         </div>
@@ -33,10 +37,11 @@ function Returns() {
           <label className="block">
             <input
               type="text"
-              id="marketVal"
+              id="currentVal"
               className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="Enter Market value of single share"
+              placeholder="Enter current Market Value of that stock"
               autoComplete="off"
+              defaultValue={0}
             />
           </label>
         </div>
@@ -47,18 +52,18 @@ function Returns() {
               type="text"
               id="avgVal"
               className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="Enter Average value of the shares bought"
+              placeholder="Enter Average value of the stock"
               autoComplete="off"
+              defaultValue={0}
+              onChange={calcReturns}
             />
           </label>
         </div>
 
-        <button
-          className="p-2 my-8 flex w-full justify-center bg-green-400 hover:bg-green-500 text-white font-medium rounded"
-          onClick={calcAvg}>
-          Average Price : <div id="avgBox"></div>
-        </button>
-      </form>
+        <div
+          className="p-2 my-8 flex w-full justify-center bg-green-400 hover:bg-green-500 text-white font-medium rounded">
+          Returns : ₹ <div id="returnsBox"></div>
+        </div>
     </>
   );
 }
