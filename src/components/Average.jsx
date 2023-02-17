3a23687c2b906b215c03445cb02fa266671f3fad
Pushkar Dangi <pushkardangi746@gmail.com>
2023-02-17 10:44:52 +0530

    replace: using state to handle values

--- src/components/Average.jsx
+++ src/components/Average.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Average() {
 
+    const [totalVal, setTotalVal] = useState("");
+    const [shares, setShares] = useState("");
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        setTotalVal(totalVal + "+");
+      }
+    };
+
     const calcAvg = (event) => {
     
-        let shares = parseInt(document.getElementById("totalShares").value);
-        let average = document.getElementById("stockVal").value;
-
-        let avg = eval(average) / shares;
-
-        document.getElementById("avgBox").innerHTML = ": ₹" + avg.toFixed(2);
-
-        event.preventDefault();
-      };
+      let avg = eval(totalVal) / shares;
+      
+      document.getElementById("avgBox").innerHTML = ": ₹" + avg.toFixed(2);
+      
+      event.preventDefault();
+    };
     
   return (
     <>
@@ -25,6 +31,8 @@ function Average() {
                 id="totalShares"
                 className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-slate-300 focus:ring-slate-300 block w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter number of stocks"
+                value={shares}
+                onChange={(event) => setShares(event.target.value)}
               />
             </label>
           </div>
@@ -34,15 +42,19 @@ function Average() {
               <input
                 type="text"
                 id="stockVal"
+                value={totalVal}
                 className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-slate-300 focus:ring-slate-300 block w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter price of every stock with + in between"
                 autoComplete="off"
+                onChange={(event) => setTotalVal(event.target.value)}
+                onKeyDown={handleKeyDown}
               />
             </label>
           </div>
 
           <div
-            className="p-2 my-8 unselectable cursor-pointer flex w-full justify-center bg-green-400 hover:bg-green-500 text-white font-medium rounded" onClick={calcAvg}>
+            className="p-2 my-8 unselectable cursor-pointer flex w-full justify-center bg-green-400 hover:bg-green-500 text-white font-medium rounded"
+            onClick={calcAvg}>
             Average Price<div id="avgBox"></div>
           </div>
 
